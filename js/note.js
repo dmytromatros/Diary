ee56bb565dc6e7bfeda256872e95583585919566
dmytromatros <dimamatroswork@gmail.com>
2022-09-07 10:45:19 +0300

    note movements done

--- js/note.js
+++ js/note.js
@@ -4,8 +4,12 @@ let textarea = document.querySelector(".textarea-note");
 let textareaHidden = document.querySelector(".textarea-note-hidden");
 let noteBottomBtn = document.querySelector(".notes-block-main__btn");
 
+let allNotes = document.querySelectorAll(".one-note");
+
 textAreaAdjust(textarea);
 fontSize();
+notemovements();
+
 window.addEventListener("resize", function () {
   containerWidth = container.offsetWidth;
   noteBottomBtn.style.width = `${containerWidth - 40}px`;
@@ -14,6 +18,7 @@ window.addEventListener("click", function () {
   containerWidth = container.offsetWidth;
   noteBottomBtn.style.width = `${containerWidth - 40}px`;
 });
+
 function textAreaAdjust(that) {
   textarea.addEventListener("keydown", () => {
     textareaHidden.value = that.value;
@@ -55,4 +60,20 @@ function fontSize() {
     textarea.style.fontSize = `${fzCount}px`;
     textareaHidden.style.fontSize = `${fzCount}px`;
   };
+}
+
+function notemovements() {
+  allNotes.forEach((e) => {
+    let mouseMove = function () {
+      e.style.position = "absolute";
+      e.style.left = event.pageX - e.offsetWidth / 1.5 + "px";
+      e.style.top = event.pageY - e.offsetHeight / 2 + "px";
+    };
+    e.addEventListener("mousedown", () => {
+      e.addEventListener("mousemove", mouseMove);
+    });
+    e.addEventListener("mouseup", () => {
+      e.removeEventListener("mousemove", mouseMove);
+    });
+  });
 }
